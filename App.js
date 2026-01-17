ccf20befcfc4f7f499233d2dca800339ce987114
yoshino <phamlinh2162005@gmail.com>
2026-01-17 12:35:20 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text>Hello, world</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'skyblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
